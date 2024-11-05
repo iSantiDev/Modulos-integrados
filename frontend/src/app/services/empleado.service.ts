d30b7ca976d84327bce88753c6b7cf7b4ec89b96
iSantiDev <santis.sierra6@gmail.com>
2024-11-04 19:23:23 -0500

    frontend listo

--- frontend/src/app/services/empleado.service.ts
+++ frontend/src/app/services/empleado.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Empleado } from '../models/empleado';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmpleadoService {
+  selectedEmpleado: Empleado;
+  empleados: Empleado[];
+  readonly URL_API = 'http://localhost:3000/api/empleados';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.selectedEmpleado = new Empleado();
+    this.empleados = [];
+  }
+
+  getEmpleados() {
+    return this.http.get(this.URL_API);
+  }
+
+  PostEmpleado(Empleado: Empleado) {
+    return this.http.post(this.URL_API, Empleado);
+  }
+
+  putEmpleado(Empleado: Empleado) {
+    return this.http.put(this.URL_API + `/${Empleado._id}`, Empleado);
+  }
+
+  deteleEmpleado(_id: string) {
+    return this.http.delete(this.URL_API + `/${_id}`);
+  }
 }
